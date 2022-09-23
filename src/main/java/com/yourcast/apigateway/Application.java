d1dcd0d6a84026063eff85eed2e65f6f06aad2a8
Dmytro Kuchura <kuchura.d@gmail.com>
2022-09-23 21:59:32 +0300

    revert

--- src/main/java/com/yourcast/apigateway/Application.java
+++ src/main/java/com/yourcast/apigateway/Application.java
@@ -2,12 +2,9 @@ package com.yourcast.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({KafkaProperties.class})
 @ConfigurationPropertiesScan
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/yourcast/apigateway/model/dto/Payload.java
+++
@@ -1,6 +0,0 @@
-package com.yourcast.apigateway.model.dto;
-
-import java.io.Serializable;
-
-public interface Payload extends Serializable {
-}
